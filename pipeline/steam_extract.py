67569c37ef069b76caec40eeb0ba5e00f51e6102
Emmanuel <dapo.12@icloud.com>
2024-04-23 18:45:03 +0100

    first

---
+++ pipeline/steam_extract.py
@@ -0,0 +1,95 @@
+from os import environ as ENV
+from dotenv import load_dotenv
+import requests as req
+from bs4 import BeautifulSoup
+
+
+def get_rating(game_soup: BeautifulSoup) -> float:
+    """Function to get the rating percentage of a game from its URL"""
+
+    rating = game_soup.find('div', class_="review_ctn")
+    if "There are no reviews for this product" in rating:
+        return 0.00
+    else:
+        positive = rating.find('label', {'for': "review_type_positive"})
+        negative = rating.find('label', {'for': "review_type_negative"})
+        if positive:
+            p_num = int(positive.find(
+                'span', class_='user_reviews_count').text.strip('()'))
+        else:
+            return 0.00
+        if negative:
+            n_num = int(negative.find(
+                'span', class_='user_reviews_count').text.strip('()'))
+        else:
+            return 100
+
+        return round((p_num/(p_num+n_num)) * 100, 2)
+
+
+def get_platforms(game_soup: BeautifulSoup) -> list:
+    """Function to get the platforms a game is available on from its URL"""
+
+    if game_soup.findAll(
+            'div', class_="sysreq_tab"):
+        platforms = [i.text.strip() for i in game_soup.findAll(
+            'div', class_="sysreq_tab")]
+        for i in range(len(platforms)):
+            if "Linux" in platforms[i]:
+                platforms[i] = "Linux"
+        return platforms
+    else:
+        platforms = game_soup.findAll(
+            'div', class_="sysreq_contents")
+        platform_list = [platform.find(
+            'ul', class_='bb_ul').text for platform in platforms if platform.find('ul', class_='bb_ul')][0]
+        if "Windows" in platform_list:
+            return "Windows"
+        if "mac" in platform_list:
+            return "macOS"
+        if "Linux" in platform_list:
+            return "Linux"
+
+
+def get_tags(game_soup: BeautifulSoup) -> list:
+
+    return [i.text.strip()
+            for i in game_soup.find_all('a', class_='app_tag')]
+
+
+def get_developer(game_soup: BeautifulSoup) -> str:
+
+    return game_soup.find('div', class_='dev_row').text.split('\n')[-2]
+
+
+if __name__ == "__main__":
+
+    load_dotenv()
+
+    res = req.get(ENV["BASE_URL"])
+    soup = BeautifulSoup(res.text, features="html.parser")
+
+    all_containers = soup.find_all(
+        'a', class_="search_result_row")
+
+    game = []
+
+    for each_container in all_containers:
+        title = each_container.find('span', class_="title").text
+        price = each_container.find('div', class_="discount_final_price").text
+        release_date = each_container.find(
+            'div', class_="col search_released responsive_secondrow").text
+        game_url = each_container['href']
+
+        game_res = req.get(game_url)
+        game_soup = BeautifulSoup(game_res.text, features="html.parser")
+
+        game_tags = get_tags(game_soup)
+        platform_list = get_platforms(game_soup)
+        rating = get_rating(game_soup)
+        developer = get_developer(game_soup)
+
+        game.append([title, price, release_date.strip(), game_url])
+        game_list = [game, platform_list, rating, developer]
+
+        print(game_list)
