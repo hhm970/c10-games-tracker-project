d2ed562de9f23c9cff424f3818ffdaa2c5325df2
howardman <howardppg@gmail.com>
2024-04-24 17:26:32 +0100

    Created primary database input function

--- pipeline/load.py
+++ pipeline/load.py
@@ -1,9 +1,13 @@
-"""Given our games data in a lists of lists format, we load this data 
-into our cloud-based database."""
+"""Given some input game data as a list[list] object, we load this data 
+into our cloud-based database. Each embedded list should have values in the 
+following order,
+
+name: str; description: str; price: float; developer: str; publisher: str; 
+release_date: datetime, rating: float, website_id: int, tags: list, platform: list 
+"""
 from os import environ as ENV
 from dotenv import load_dotenv
 
-from boto3 import client
 from psycopg2 import connect
 from psycopg2.extras import RealDictCursor, execute_values
 from psycopg2.extensions import connection
@@ -20,6 +24,28 @@ def get_db_connection(config) -> connection:
         port=config.get("DB_PORT", 5432),
         cursor_factory=RealDictCursor
     )
+
+
+def input_game_into_db(game_data: list[list], conn: connection) -> None:
+    """Given our game data, we insert each row into the game table, excluding
+    the tags and the platforms."""
+    with conn.cursor() as cur:
+        cur.executemany(
+            """INSERT INTO (name, description, price, developer, publisher, release_date, rating, website_id)
+            VALUES %s""", game_data[:-2])
+
+        cur.close()
+        conn.commit()
+
+
+def input_game_tags_into_db(game_data: list[list], conn: connection) -> None:
+    """"""
+    pass
+
+
+def input_game_plat_into_db(game_data: list[list], conn: connection) -> None:
+    """"""
+    pass
 
 
 if __name__ == "__main__":
