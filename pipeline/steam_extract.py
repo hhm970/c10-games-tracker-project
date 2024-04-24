0d1704cae8702845ef92411279c3159a29dc4fff
Emmanuel <dapo.12@icloud.com>
2024-04-24 11:25:31 +0100

    add description

--- pipeline/steam_extract.py
+++ pipeline/steam_extract.py
@@ -2,6 +2,8 @@ from os import environ as ENV
 from dotenv import load_dotenv
 import requests as req
 from bs4 import BeautifulSoup
+from datetime import date
+from time import sleep
 
 
 def get_rating(game_soup: BeautifulSoup) -> float:
@@ -49,6 +51,8 @@ def get_platforms(game_soup: BeautifulSoup) -> list:
             return "macOS"
         if "Linux" in platform_list:
             return "Linux"
+        else:
+            return "Windows"
 
 
 def get_tags(game_soup: BeautifulSoup) -> list:
@@ -62,6 +66,63 @@ def get_developer(game_soup: BeautifulSoup) -> str:
     return game_soup.find('div', class_='dev_row').text.split('\n')[-2]
 
 
+def get_name_price_date(container: BeautifulSoup) -> list:
+
+    title = container.find('span', {'class': 'title'}).text
+    price = container.find('div', {'class': "discount_final_price"}).text
+    release_date = container.find(
+        'div', {'class': "col search_released responsive_secondrow"}).text
+
+    return [title, price, release_date.strip()]
+
+
+def get_platform_ids(platform: list) -> list:
+    '''Returns a list of platform IDs given a
+    string of playable platforms.'''
+    id_list = []
+    if 'Windows' in platform:
+        id_list.append(1)
+    if 'macOS' in platform:
+        id_list.append(2)
+    if 'Linux' in platform:
+        id_list.append(3)
+    return id_list
+
+
+def get_each_game_details(game_url: str) -> list:
+
+    game_res = req.get(game_url)
+    game_soup = BeautifulSoup(game_res.text, features="html.parser")
+
+    game_tags = get_tags(game_soup)
+    platform_list = get_platforms(game_soup)
+    platform_id_list = get_platform_ids(platform_list)
+    rating = get_rating(game_soup)
+    developer = get_developer(game_soup)
+    publisher = 'x'
+
+    return [developer, publisher, date.today(), rating, 1, game_tags, platform_id_list]
+
+
+def get_everything(soup: BeautifulSoup) -> list:
+
+    final_list = []
+
+    for container in all_containers:
+
+        name_price_date_list = get_name_price_date(container)
+        game_url = container['href']
+        try:
+            detail_list = get_each_game_details(game_url)
+            print([name_price_date_list, detail_list])
+            final_list.append([name_price_date_list, detail_list])
+            sleep(1)
+        except IndexError:
+            continue
+
+    return final_list
+
+
 if __name__ == "__main__":
 
     load_dotenv()
@@ -72,24 +133,4 @@ if __name__ == "__main__":
     all_containers = soup.find_all(
         'a', class_="search_result_row")
 
-    game = []
-
-    for each_container in all_containers:
-        title = each_container.find('span', class_="title").text
-        price = each_container.find('div', class_="discount_final_price").text
-        release_date = each_container.find(
-            'div', class_="col search_released responsive_secondrow").text
-        game_url = each_container['href']
-
-        game_res = req.get(game_url)
-        game_soup = BeautifulSoup(game_res.text, features="html.parser")
-
-        game_tags = get_tags(game_soup)
-        platform_list = get_platforms(game_soup)
-        rating = get_rating(game_soup)
-        developer = get_developer(game_soup)
-
-        game.append([title, price, release_date.strip(), game_url])
-        game_list = [game, platform_list, rating, developer]
-
-        print(game_list)
+    print(get_everything(all_containers))
