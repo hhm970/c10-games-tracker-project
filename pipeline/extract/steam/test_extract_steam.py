36e5bd1405d974f302ab2461a2b77f77c045b92f
annalisev <149485371+annalisev@users.noreply.github.com>
2024-04-25 17:10:32 +0100

    Update pipeline/extract/steam/test_extract_steam.py

--- pipeline/extract/steam/test_extract_steam.py
+++ pipeline/extract/steam/test_extract_steam.py
@@ -1,4 +1,4 @@
-"""Test file for steam_extract.py"""
+"""Test file for steam_extract.py."""
 
 from steam_extract import get_rating, get_platform_ids, get_tags, get_developer, get_publisher, get_name_price_date
 
