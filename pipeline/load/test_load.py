cf54ac7fa83658c3967abf3aba538b1c89327669
howardman <howardppg@gmail.com>
2024-04-25 15:23:43 +0100

    Fixed error with test_load.py

--- pipeline/load/test_load.py
+++ pipeline/load/test_load.py
@@ -10,6 +10,3 @@ from load import (format_release_date_dt, input_game_into_db,
 def test_format_release_date_dt(test_game_data):
     formatted_game_data = format_release_date_dt(test_game_data)
     assert isinstance(formatted_game_data[0][5], datetime) == True
-
-
-def test_input_game_into_db_success
