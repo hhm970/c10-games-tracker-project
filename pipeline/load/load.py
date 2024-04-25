bcf0e9851d99fc0417e7a721675572360617a5b6
howardman <howardppg@gmail.com>
2024-04-25 11:26:55 +0100

    Allocated all load files into a load directory

--- pipeline/load/load.py
+++ pipeline/load/load.py
@@ -44,12 +44,13 @@ def input_game_into_db(game_data: list[list], conn: connection) -> None:
 def input_game_plat_into_db(game_data: list[list], conn: connection) -> None:
     """For each game in our input data, we input all of its supported platforms
     into the platform_assignment table."""
+
     with conn.cursor() as cur:
         for game in game_data:
             cur.execute("""SELECT game_id FROM game
                         WHERE name = %s""", (game_data[0],))
 
-            game_id = cur.fetchone()[0]
+            game_id = cur.fetchone()
             game_plat_list = game[-1]
 
             for plat in game_plat_list:
@@ -61,8 +62,36 @@ def input_game_plat_into_db(game_data: list[list], conn: connection) -> None:
 
 
 def input_game_tags_into_db(game_data: list[list], conn: connection) -> None:
-    """"""
-    pass
+    """For each game, we iterate through its tags. We use the PostgreSQL extension
+    pg_tgm to measure similarity of the tags with existing entries in the tag table,
+    appending the tag iterand into the table if no similar entries are detected."""
+
+    with conn.cursor() as cur:
+        for game in game_data:
+            cur.execute("""SELECT game_id FROM game
+                        WHERE name = %s""", (game_data[0],))
+
+            game_id = cur.fetchone()
+            game_tags = game[-2]
+
+            for tag in game_tags:
+                cur.execute("""SELECT tag_id FROM tag
+                            WHERE SIMILARITY(%s, tag_name) > 0.8""", (tag,))
+                tag_id_match = cur.fetchone()
+
+                if tag_id_match is None:
+                    cur.execute("""INSERT INTO tag (tag_name)
+                                VALUES %s""", (tag,))
+
+                    cur.execute("""SELECT tag_id FROM tag
+                                WHERE tag_name = %s""", (tag,))
+
+                    tag_id_match = cur.fetchone()
+
+                cur.execute("""INSERT INTO game_tag_matching
+                            VALUES %s""", (game_id, tag_id_match))
+        cur.close()
+    conn.commit()
 
 
 if __name__ == "__main__":
---
+++ pipeline/load/sample_tags.py
@@ -0,0 +1,44 @@
+"""A sample collection of tags from the Steam, GOG and EpicGames websites."""
+
+
+gog_sample_tags = {'Nature', 'Local Co-Op', 'Tactical RPG', 'Indie', 'Base Building',
+                   'Puzzle', 'Logic', 'Fantasy', 'Combat', 'Turn-Based', 'Tactical',
+                   'Role-playing', 'Relaxing', 'Post-apocalyptic', 'Family Friendly',
+                   'Pixel Graphics', 'Perma Death', '2D', 'Local Multiplayer', 'Cozy',
+                   'Casual', 'Multiplayer', 'Roguelite', 'Dark', 'Action', 'Non Violent',
+                   'Strategy', 'Adventure', 'Story Rich', 'Atmospheric', 'Sci-fi'}
+
+
+epic_sample_tags = {'Action', 'Action-Adventure', 'Adventure', 'Card Game', 'Casual',
+                    'City Builder', 'Comedy', 'Dungeon Crawler', 'Exploration', 'Fantasy',
+                    'Fighting', 'First Person', 'Horror', 'Indie', 'MOBA', 'Music',
+                    'Narration', 'Open World', 'Party', 'Platformer', 'Puzzle', 'Racing',
+                    'Retro', 'Rogue-Lite', 'RPG', 'RTS', 'Shooter', 'Simulation', 'Space',
+                    'Sports', 'Stealth', 'Strategy', 'Survival', 'Tower Defense', 'Trivia',
+                    'Turn-Based', 'Turn-Based Strategy', 'Achievements',
+                    'Alexa Game Control', 'Cloud Saves', 'Co-op', 'Competitive',
+                    'Controller Support', 'Cross Platform', 'Local Multiplayer', 'MMO',
+                    'Multiplayer', 'Online Multiplayer', 'Single Player', 'VR',
+                    'NFT BLOCKCHAIN', 'EARLY ACCESS'}
+
+
+steam_sample_tags = {'Audio Production', 'Cinematic', 'Indie', 'Roguelite', 'RPG',
+                     'Action-Adventure', 'Character Customization', 'Action',
+                     'Relaxing', 'Immersive Sim', 'Old School', 'Life Sim', 'Chess',
+                     'First-Person', 'Traditional Roguelike', 'Singleplayer', 'Arcade',
+                     'RTS', 'Physics', 'Turn-Based Strategy', 'Fantasy',
+                     'Roguelike Deckbuilder', 'Shooter', 'Boomer Shooter',
+                     'City Builder', 'Base Building', 'Zombies', 'Match 3',
+                     'Card Game', 'Roguelike', 'Assassin', 'Souls-like', 'Modern',
+                     'Comedy', 'Drama', 'Action Roguelike', 'Logic', 'Atmospheric',
+                     'Adventure', 'Strategy', 'Combat', 'Hidden Object', '2D', 'Horror',
+                     'Turn-Based Tactics', 'Auto Battler', 'Colorful', 'Strategy RPG',
+                     'Dark', 'Hand-drawn', 'Dungeon Crawler', 'Story Rich', 'Cartoony',
+                     'Agriculture', 'Farming Sim', 'Tactical', 'Realistic', 'Cute',
+                     'Walking Simulator', 'Animation & Modeling', 'Text-Based',
+                     'Resource Management', 'Casual', 'Deckbuilding', 'Hero Shooter',
+                     'Post-apocalyptic', 'Collectathon', 'Puzzle', 'Photo Editing',
+                     'Nature', 'Spectacle fighter', 'Turn-Based Combat', 'Survival',
+                     'Choices Matter', 'Building', 'Inventory Management',
+                     'Female Protagonist', '3D', 'Sports', 'Card Battler', 'Typing',
+                     'Bullet Hell', 'Magic', 'Tactical RPG', 'Simulation', 'Board Game'}
