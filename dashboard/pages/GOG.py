facba89232321762d8a2cebe070b423f352d7b40
annalisev <annaliseverzijl@gmail.com>
2024-04-29 10:32:06 +0100

    Change navigation station

--- dashboard/pages/GOG.py
+++ dashboard/pages/GOG.py
@@ -17,3 +17,8 @@ if __name__ == "__main__":
     with st.sidebar:
         st.title("Navigation Station :rocket:")
         st.write("---")
+        st.page_link("Home.py")
+        st.page_link("pages/Epic.py")
+        st.page_link("pages/GOG.py")
+        st.page_link("pages/Steam.py")
+        st.page_link("pages/Subscribe.py")
